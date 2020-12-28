aa96806e5fc1b52890663ed1c8aed327068e732d
Everardo Ortiz <everardo.ortiz.5@gmail.com>
2020-12-28 18:59:27 +0800

    add images file and js file

---
+++ script.js
@@ -0,0 +1,7 @@
+// add the scroll effect to the nav bar
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+}); 
